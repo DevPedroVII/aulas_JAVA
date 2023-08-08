5c153609f1b8fc3002636ffb69dde8862f6dca65
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-07 23:19:54 -0300

    Add files via upload

--- JAVAS/CursioJava/src/packg/Programa1.java
+++ JAVAS/CursioJava/src/packg/Programa1.java
@@ -3,8 +3,18 @@ package packg;
 public class Programa1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int numero = 5;
+		if (numero > 5) {
+			System.out.println("O numero " +  numero  + " é maior que 5");
+		}else if (numero == 5) {
+			System.out.println("O numero " +  numero  + " é igual a 5" );
+		}else if (numero % 2 == 0){
+			System.out.println("o numero " + numero + " é par");
+		}
+		else{
+			System.out.println("O numero " +  numero  + " menor que 5" );
+		}
+	
 	}
 
 }
