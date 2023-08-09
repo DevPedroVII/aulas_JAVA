50945895a8034a823e7e3af390688ea4faf816a7
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-08 22:53:17 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ JAVAS/projetin/src/packzin/Veridade.java
@@ -0,0 +1,12 @@
+//  exemplo usando o operador ternário
+
+package packzin;
+
+public class Veridade {
+	public static void main(String[] args) {
+		int idade = 18;
+		String status = (idade >= 18) ? "adulto" : "Falso";
+		System.out.println(status);
+	}
+
+}
