b84020da5f1e01c5a1e228b67841a95b7c6a5d62
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-08 22:33:36 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ JAVAS/projetin/src/packzin/Programa05.java
@@ -0,0 +1,20 @@
+//Operador ternario
+
+//main + Ctrl Space
+package packzin;
+
+public class Programa05 {
+	
+	public static void main(String[] args) {
+		int valor = 3, numero;
+		
+		if (valor > 0 ) {
+			numero = valor;
+		}else { 
+			numero = 7;
+		}
+		
+		System.out.println();
+	}
+
+}
