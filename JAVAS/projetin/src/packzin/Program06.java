da8ef29a441e8b2154ff61b8eb397b8660ff3028
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-09 23:30:45 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ JAVAS/projetin/src/packzin/Program06.java
@@ -0,0 +1,25 @@
+//Switch
+package packzin;
+
+public class Program06 {
+	
+	public static void main(String[] args) {
+		int numero = 2;	
+		
+		switch (numero) {
+			case 0:
+				System.out.println("numero é 0");
+				break;
+			case 3:
+				System.out.println("numero é 3");
+				break;
+			case  5:
+				System.out.println("numero é 5");
+				break;
+		default:
+			System.out.println("numero é" + numero);
+			break;
+		}
+	}
+
+}
