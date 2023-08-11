02930cfa68eebfca9a34a2f7a9e600c6b44abddb
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-11 00:50:15 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/Program007.java
+++ JAVAS/projetin/src/packzin/Program007.java
@@ -7,12 +7,13 @@ import java.util.Scanner;
 public class Program007 {
 	public static void main(String[] args) {
 		
-		int idade;
+		int idade = 1;
 		String nome;
 		
 		//recebe Dados do usuario via teclado.
 		Scanner teclado = new Scanner(System.in);
 		
+		while(idade > 0) {
 		System.out.println("Qual o seu nome?");
 		nome = teclado.nextLine();
 		
