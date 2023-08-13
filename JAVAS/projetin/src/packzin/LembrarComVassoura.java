2fc926997e238c5f6ebc670efa6d113ee343ad9f
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-12 22:53:50 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ JAVAS/projetin/src/packzin/LembrarComVassoura.java
@@ -0,0 +1,35 @@
+package packzin;
+
+import java.util.Scanner;
+
+public class LembrarComVassoura {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean viVassoura = false; // Inicialmente, você não viu a vassoura
+        boolean lembrou = false; // Inicialmente, você não lembrou
+
+        System.out.println("Você vê a vassoura? (sim/não)");
+        String resposta = scanner.nextLine();
+
+        if (resposta.equalsIgnoreCase("sim")) {
+            viVassoura = true;
+        }
+
+        while (!lembrou) {
+            if (viVassoura) {
+                System.out.println("Você se lembrou daquela situação!");
+                lembrou = true;
+            } else {
+                System.out.println("Você não consegue se lembrar, talvez se você ver a vassoura...");
+                System.out.println("Você vê a vassoura agora? (sim/não)");
+                resposta = scanner.nextLine();
+                if (resposta.equalsIgnoreCase("sim")) {
+                    viVassoura = true;
+                }
+            }
+        }
+
+        System.out.println("Fim do programa.");
+    }
+}
+
