b5a2fa57a70cf6e78cc3dec3eddda7d310092f89
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-18 23:12:58 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ JAVAS/projetin/src/packzin/SimuladorCampeonato.java
@@ -0,0 +1,62 @@
+package packzin;
+
+import java.util.Random;
+
+public class SimuladorCampeonato {
+    public static void main(String[] args) {
+        final int NUM_TEAMS = 20;
+        final int NUM_ROUNDS = 38;
+        final int POINTS_WIN = 3;
+        final int POINTS_DRAW = 1;
+        final int POINTS_LOSS = 0;
+
+        Random random = new Random();
+        int[][] tabela = new int[NUM_TEAMS][2];  // Array para armazenar pontos e saldo de gols
+
+        for (int i = 0; i < NUM_TEAMS; i++) {
+            tabela[i][0] = random.nextInt(50);  // Inicializa a pontuação aleatoriamente
+        }
+
+        for (int round = 1; round <= NUM_ROUNDS; round++) {
+            System.out.println("Rodada " + round + ":");
+
+            for (int i = 0; i < NUM_TEAMS; i++) {
+                int resultado = random.nextInt(3);  // 0: derrota, 1: empate, 2: vitória
+
+                if (resultado == 0) {
+                    tabela[i][0] += POINTS_LOSS;
+                } else if (resultado == 1) {
+                    tabela[i][0] += POINTS_DRAW;
+                } else {
+                    tabela[i][0] += POINTS_WIN;
+                }
+            }
+
+            // Ordena a tabela classificatória com base nos pontos
+            java.util.Arrays.sort(tabela, (a, b) -> Integer.compare(b[0], a[0]));
+        }
+
+        System.out.println("\nClassificação Final:");
+
+        for (int i = 0; i < NUM_TEAMS; i++) {
+            String posicao = String.format("%2d", i + 1);
+            System.out.println(posicao + "ª - Equipe " + (i + 1) + ": " + tabela[i][0] + " pontos");
+        }
+
+        // Imprime os times que se classificaram para Libertadores, Sul-Americana e rebaixamento
+        System.out.println("\nClassificados para a Libertadores:");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Equipe " + (i + 1));
+        }
+
+        System.out.println("\nClassificados para a Sul-Americana:");
+        for (int i = 4; i < 16; i++) {
+            System.out.println("Equipe " + (i + 1));
+        }
+
+        System.out.println("\nEquipes Rebaixadas:");
+        for (int i = NUM_TEAMS - 4; i < NUM_TEAMS; i++) {
+            System.out.println("Equipe " + (i + 1));
+        }
+    }
+}
