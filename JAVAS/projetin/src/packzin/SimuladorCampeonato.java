5eafddf5046a6a3659608caf9194052d43fd98c1
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-18 23:15:45 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/SimuladorCampeonato.java
+++ JAVAS/projetin/src/packzin/SimuladorCampeonato.java
@@ -1,7 +1,7 @@
 package packzin;
 
 import java.util.Random;
-
+//ATT 1.0
 public class SimuladorCampeonato {
     public static void main(String[] args) {
         final int NUM_TEAMS = 20;
@@ -11,52 +11,56 @@ public class SimuladorCampeonato {
         final int POINTS_LOSS = 0;
 
         Random random = new Random();
-        int[][] tabela = new int[NUM_TEAMS][2];  // Array para armazenar pontos e saldo de gols
+        int[][] tabela = new int[NUM_TEAMS][3];  // Array para armazenar pontos, saldo de gols e identificador da equipe
 
+        // Inicializa a tabela com identificadores de equipe e pontos aleatórios
         for (int i = 0; i < NUM_TEAMS; i++) {
-            tabela[i][0] = random.nextInt(50);  // Inicializa a pontuação aleatoriamente
+            tabela[i][0] = i;  // Identificador da equipe
+            tabela[i][1] = random.nextInt(50);  // Pontuação aleatória
         }
 
+        // Simulação das rodadas
         for (int round = 1; round <= NUM_ROUNDS; round++) {
             System.out.println("Rodada " + round + ":");
 
+            // Simula o desempenho das equipes na rodada
             for (int i = 0; i < NUM_TEAMS; i++) {
                 int resultado = random.nextInt(3);  // 0: derrota, 1: empate, 2: vitória
 
                 if (resultado == 0) {
-                    tabela[i][0] += POINTS_LOSS;
+                    tabela[i][1] += POINTS_LOSS;
                 } else if (resultado == 1) {
-                    tabela[i][0] += POINTS_DRAW;
+                    tabela[i][1] += POINTS_DRAW;
                 } else {
-                    tabela[i][0] += POINTS_WIN;
+                    tabela[i][1] += POINTS_WIN;
                 }
             }
-
-            // Ordena a tabela classificatória com base nos pontos
-            java.util.Arrays.sort(tabela, (a, b) -> Integer.compare(b[0], a[0]));
         }
 
-        System.out.println("\nClassificação Final:");
+        // Ordena a tabela classificatória com base nos pontos
+        java.util.Arrays.sort(tabela, (a, b) -> Integer.compare(b[1], a[1]));
 
+        // Imprime a classificação final
+        System.out.println("\nClassificação Final:");
         for (int i = 0; i < NUM_TEAMS; i++) {
             String posicao = String.format("%2d", i + 1);
-            System.out.println(posicao + "ª - Equipe " + (i + 1) + ": " + tabela[i][0] + " pontos");
+            System.out.println(posicao + "ª - Equipe " + (tabela[i][0] + 1) + ": " + tabela[i][1] + " pontos");
         }
 
         // Imprime os times que se classificaram para Libertadores, Sul-Americana e rebaixamento
         System.out.println("\nClassificados para a Libertadores:");
         for (int i = 0; i < 4; i++) {
-            System.out.println("Equipe " + (i + 1));
+            System.out.println("Equipe " + (tabela[i][0] + 1));
         }
 
         System.out.println("\nClassificados para a Sul-Americana:");
         for (int i = 4; i < 16; i++) {
-            System.out.println("Equipe " + (i + 1));
+            System.out.println("Equipe " + (tabela[i][0] + 1));
         }
 
         System.out.println("\nEquipes Rebaixadas:");
         for (int i = NUM_TEAMS - 4; i < NUM_TEAMS; i++) {
-            System.out.println("Equipe " + (i + 1));
+            System.out.println("Equipe " + (tabela[i][0] + 1));
         }
     }
 }
