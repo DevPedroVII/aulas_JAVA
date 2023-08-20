c438bdf7cfd73469337dc39e74a883e373ede3f3
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-19 23:27:46 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/SistemaEscolar.java
+++ JAVAS/projetin/src/packzin/SistemaEscolar.java
@@ -1,33 +1,35 @@
 package packzin;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class SistemaEscolar {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.println("=== Sistema Escolar ===");
         System.out.print("Informe o nome do aluno: ");
         String nomeAluno = scanner.nextLine();
-
+        
         System.out.print("Informe a série (1 para Fundamental ou 2 para Médio): ");
         int serie = scanner.nextInt();
-
+        
         Aluno aluno = new Aluno(nomeAluno, serie);
-
+        
         aluno.mostrarFichaMatricula();
-
+        
         for (String materia : aluno.getMaterias()) {
             System.out.println("Matéria: " + materia);
             for (int unidade = 1; unidade <= 3; unidade++) {
-                System.out.print("Informe a nota da unidade " + unidade + " para " + materia + ": ");
+                System.out.print("Informe a nota da unidade " + unidade + ": ");
                 double nota = scanner.nextDouble();
                 aluno.addNota(materia, unidade, nota);
             }
         }
-
-        aluno.calcularMediaPorMateria();
+        
+        aluno.calcularMedias();
         aluno.verificarAprovacao();
     }
 }
@@ -35,9 +37,10 @@ public class SistemaEscolar {
 class Aluno {
     private String nome;
     private int serie;
-    private double mediaMinima = 7;
-    private Map<String, List<Double>> notasPorMateria;
-
+    private Map<String, Map<Integer, Double>> notas;
+    private double mediaRecuperacao = 6;
+    private double mediaAnual = 7;
+    
     public static final String[] MATERIAS_FUNDAMENTAL = {
         "Língua Portuguesa", "Técnicas de redação", "Matemática", "História", "Geografia",
         "Ciências (química e física)", "Artes", "Inglês", "Espanhol", "Geometria", "Sociologia", "Filosofia", "Ed. Fisica"
@@ -51,9 +54,10 @@ class Aluno {
     public Aluno(String nome, int serie) {
         this.nome = nome;
         this.serie = serie;
-        this.notasPorMateria = new HashMap<>();
+        this.notas = new HashMap<>();
+        
         for (String materia : getMaterias()) {
-            notasPorMateria.put(materia, new ArrayList<>());
+            notas.put(materia, new HashMap<>());
         }
     }
 
@@ -70,41 +74,42 @@ class Aluno {
     }
 
     public void addNota(String materia, int unidade, double nota) {
-        if (notasPorMateria.containsKey(materia)) {
-            List<Double> notas = notasPorMateria.get(materia);
-            notas.add(nota);
+        Map<Integer, Double> unidadeNotas = notas.get(materia);
+        if (unidadeNotas != null) {
+            unidadeNotas.put(unidade, nota);
         }
     }
 
-    public void calcularMediaPorMateria() {
-        for (String materia : notasPorMateria.keySet()) {
-            List<Double> notas = notasPorMateria.get(materia);
-            if (!notas.isEmpty()) {
-                double soma = 0;
-                for (double nota : notas) {
-                    soma += nota;
-                }
-                double media = soma / notas.size();
-                System.out.println("Média de " + materia + ": " + media);
+    public void calcularMedias() {
+        for (String materia : notas.keySet()) {
+            Map<Integer, Double> unidadeNotas = notas.get(materia);
+            double soma = 0;
+            int count = 0;
+            for (double nota : unidadeNotas.values()) {
+                soma += nota;
+                count++;
             }
+            double media = soma / count;
+            System.out.println("Média de " + materia + ": " + media);
+            unidadeNotas.put(0, media); // Armazenar a média anual na posição 0
         }
     }
 
     public void verificarAprovacao() {
-        for (String materia : notasPorMateria.keySet()) {
-            List<Double> notas = notasPorMateria.get(materia);
-            if (!notas.isEmpty()) {
-                double soma = 0;
-                for (double nota : notas) {
-                    soma += nota;
-                }
-                double media = soma / notas.size();
-                if (media >= mediaMinima) {
-                    System.out.println("Parabéns! " + nome + " passou em " + materia + " com média " + media);
-                } else {
-                    System.out.println("Infelizmente, " + nome + " não passou em " + materia + " com média " + media);
-                }
+        boolean passouDeAno = true;
+        
+        for (String materia : notas.keySet()) {
+            double media = notas.get(materia).get(0); // Pegar a média anual
+            if (media < mediaAnual) {
+                passouDeAno = false;
+                break;
             }
+        }
+
+        if (passouDeAno) {
+            System.out.println("Parabéns! " + nome + " passou de ano!");
+        } else {
+            System.out.println("Você não atingiu a média necessária e está reprovado(a). Estude mais para o próximo ano!");
         }
     }
 }
