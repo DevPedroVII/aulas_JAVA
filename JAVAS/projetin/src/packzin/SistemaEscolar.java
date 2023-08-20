6cbf1062e544d2ed6a6f7d04b8f4997d68623ca8
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-19 23:15:42 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/SistemaEscolar.java
+++ JAVAS/projetin/src/packzin/SistemaEscolar.java
@@ -1,61 +1,43 @@
 package packzin;
 
-
-import java.util.Scanner;
+import java.util.*;
 
 public class SistemaEscolar {
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-			System.out.println("=== Sistema Escolar ===");
-			System.out.print("Informe o nome do aluno: ");
-			String nomeAluno = scanner.nextLine();
-			
-			System.out.print("Informe a série (1 para Fundamental ou 2 para Médio): ");
-			int serie = scanner.nextInt();
-			
-			Aluno aluno = new Aluno(nomeAluno, serie);
-			
-			aluno.mostrarFichaMatricula();
-			
-			for (String materia : aluno.getMaterias()) {
-			    System.out.println("Matéria: " + materia);
-			    for (int unidade = 1; unidade <= 3; unidade++) {
-			        System.out.print("Informe a nota da unidade " + unidade + ": ");
-			        double nota = scanner.nextDouble();
-			        aluno.addNota(materia, unidade, nota);
-			    }
-			}
-			
-			System.out.print("O aluno fez segunda chamada? (S/N): ");
-			char segundaChamada = scanner.next().charAt(0);
-			
-			if (segundaChamada == 'S' || segundaChamada == 's') {
-			    System.out.print("Informe a matéria da segunda chamada: ");
-			    scanner.nextLine(); // Limpar o buffer
-			    String materiaSegundaChamada = scanner.nextLine();
-			    
-			    System.out.print("Informe a nota na prova de segunda chamada: ");
-			    double notaSegundaChamada = scanner.nextDouble();
-			    
-			    aluno.processarSegundaChamada(materiaSegundaChamada, notaSegundaChamada);
-			}
-			
-			aluno.calcularMedia();
-			aluno.verificarAprovacao();
-		}
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Sistema Escolar ===");
+        System.out.print("Informe o nome do aluno: ");
+        String nomeAluno = scanner.nextLine();
+
+        System.out.print("Informe a série (1 para Fundamental ou 2 para Médio): ");
+        int serie = scanner.nextInt();
+
+        Aluno aluno = new Aluno(nomeAluno, serie);
+
+        aluno.mostrarFichaMatricula();
+
+        for (String materia : aluno.getMaterias()) {
+            System.out.println("Matéria: " + materia);
+            for (int unidade = 1; unidade <= 3; unidade++) {
+                System.out.print("Informe a nota da unidade " + unidade + " para " + materia + ": ");
+                double nota = scanner.nextDouble();
+                aluno.addNota(materia, unidade, nota);
+            }
+        }
+
+        aluno.calcularMediaPorMateria();
+        aluno.verificarAprovacao();
     }
 }
 
 class Aluno {
     private String nome;
     private int serie;
-    private int unidades = 3;
-    private double mediaRecuperacao = 6;
-    private double mediaAnual = 7;
-    private double[][] notas;
-    private String[] atividades;
-    
+    private double mediaMinima = 7;
+    private Map<String, List<Double>> notasPorMateria;
+
     public static final String[] MATERIAS_FUNDAMENTAL = {
         "Língua Portuguesa", "Técnicas de redação", "Matemática", "História", "Geografia",
         "Ciências (química e física)", "Artes", "Inglês", "Espanhol", "Geometria", "Sociologia", "Filosofia", "Ed. Fisica"
@@ -69,8 +51,10 @@ class Aluno {
     public Aluno(String nome, int serie) {
         this.nome = nome;
         this.serie = serie;
-        this.notas = new double[MATERIAS_FUNDAMENTAL.length][3]; // 3 unidades
-        this.setAtividades(new String[MATERIAS_FUNDAMENTAL.length]);
+        this.notasPorMateria = new HashMap<>();
+        for (String materia : getMaterias()) {
+            notasPorMateria.put(materia, new ArrayList<>());
+        }
     }
 
     public String[] getMaterias() {
@@ -86,79 +70,41 @@ class Aluno {
     }
 
     public void addNota(String materia, int unidade, double nota) {
-        int index = findMateriaIndex(materia);
-        if (index != -1) {
-            this.notas[index][unidade - 1] = nota;
+        if (notasPorMateria.containsKey(materia)) {
+            List<Double> notas = notasPorMateria.get(materia);
+            notas.add(nota);
         }
     }
 
-    public void processarSegundaChamada(String materia, double notaSegundaChamada) {
-        int index = findMateriaIndex(materia);
-        if (index != -1) {
-            if (notaSegundaChamada <= 4) {
-                this.notas[index][0] += notaSegundaChamada; // Adiciona à primeira unidade
-                System.out.println("Nota da segunda chamada adicionada à matéria " + materia);
-            } else {
-                System.out.println("Algo de errado aconteceu na segunda chamada de " + materia);
+    public void calcularMediaPorMateria() {
+        for (String materia : notasPorMateria.keySet()) {
+            List<Double> notas = notasPorMateria.get(materia);
+            if (!notas.isEmpty()) {
+                double soma = 0;
+                for (double nota : notas) {
+                    soma += nota;
+                }
+                double media = soma / notas.size();
+                System.out.println("Média de " + materia + ": " + media);
             }
         }
-    }
-
-    public double calcularMedia() {
-        double soma = 0;
-        int count = 0;
-        for (double[] unidadeNotas : notas) {
-            for (double nota : unidadeNotas) {
-                soma += nota;
-                count++;
-            }
-        }
-        double media = soma / count;
-
-        if (media < mediaAnual && unidades > 1) {
-            try (Scanner scanner = new Scanner(System.in)) {
-				System.out.print("Informe a nota da recuperação: ");
-				double notaRecuperacao = scanner.nextDouble();
-
-				media = (media + notaRecuperacao) / 2;
-				unidades--;
-				System.out.println("Aluno em recuperação. Nova média: " + media);
-				if (notaRecuperacao == 10) {
-				    System.out.println("Parabéns! Você ganhou um presente misterioso!");
-				}
-			}
-        } else {
-            System.out.println("Média anual do aluno: " + media);
-        }
-		return media;
     }
 
     public void verificarAprovacao() {
-        double media = (double) calcularMedia();
-
-        if (media >= mediaAnual) {
-            System.out.println("Parabéns! " + nome + " passou de ano!");
-        } else if (media >= mediaRecuperacao) {
-            System.out.println("Aluno em recuperação. Média: " + media);
-        } else {
-            System.out.println("Você não atingiu a média necessária e está reprovado(a). Estude mais para o próximo ano!");
-        }
-    }
-
-    private int findMateriaIndex(String materia) {
-        for (int i = 0; i < MATERIAS_FUNDAMENTAL.length; i++) {
-            if (getMaterias()[i].equalsIgnoreCase(materia)) {
-                return i;
+        for (String materia : notasPorMateria.keySet()) {
+            List<Double> notas = notasPorMateria.get(materia);
+            if (!notas.isEmpty()) {
+                double soma = 0;
+                for (double nota : notas) {
+                    soma += nota;
+                }
+                double media = soma / notas.size();
+                if (media >= mediaMinima) {
+                    System.out.println("Parabéns! " + nome + " passou em " + materia + " com média " + media);
+                } else {
+                    System.out.println("Infelizmente, " + nome + " não passou em " + materia + " com média " + media);
+                }
             }
         }
-        return -1;
     }
-
-	public String[] getAtividades() {
-		return atividades;
-	}
-
-	public void setAtividades(String[] atividades) {
-		this.atividades = atividades;
-	}
 }
