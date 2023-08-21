51f69a2bf2642e332b47af5a1fe5ec38df0c1b67
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-20 23:42:07 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/RandomPasswordGenerator.java
+++ JAVAS/projetin/src/packzin/RandomPasswordGenerator.java
@@ -1,9 +1,7 @@
 package packzin;
 
 import java.security.SecureRandom;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class RandomPasswordGenerator {
