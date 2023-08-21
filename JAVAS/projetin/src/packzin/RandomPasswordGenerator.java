8b77a6cb701ef5d34567f86fa097fc1ca270fc8c
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-20 23:41:01 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/RandomPasswordGenerator.java
+++ JAVAS/projetin/src/packzin/RandomPasswordGenerator.java
@@ -2,28 +2,38 @@ package packzin;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RandomPasswordGenerator {
     private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    private static final int PASSWORD_LENGTH = 10;
+    private static final int PASSWORD_LENGTH = 12;
     private static final int MAX_PASSWORDS = 40;
 
+    private static SecureRandom random = new SecureRandom();
+    private static Set<String> generatedPasswords = new HashSet<>();
+
     public static void main(String[] args) {
-        List<String> generatedPasswords = new ArrayList<>();
-        SecureRandom random = new SecureRandom();
-
         while (generatedPasswords.size() < MAX_PASSWORDS) {
-            StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
-            for (int i = 0; i < PASSWORD_LENGTH; i++) {
-                int randomIndex = random.nextInt(CHARACTERS.length());
-                password.append(CHARACTERS.charAt(randomIndex));
-            }
-
-            if (!generatedPasswords.contains(password.toString())) {
-                generatedPasswords.add(password.toString());
+            String password = generateRandomPassword();
+            if (!generatedPasswords.contains(password)) {
+                generatedPasswords.add(password);
                 System.out.println("Generated password: " + password);
             }
         }
+        
+        System.out.println("Maximum number of passwords generated (" + MAX_PASSWORDS + ").");
+        System.out.println("You can find the password generator here: https://github.com/DevPedroVII");
+    }
+
+    private static String generateRandomPassword() {
+        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            password.append(randomChar);
+        }
+        return password.toString();
     }
 }
