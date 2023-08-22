96419e468a55cd4acd2a8e9c64c6159a29603c5c
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-21 21:10:43 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/MeuFi.java
+++ JAVAS/projetin/src/packzin/MeuFi.java
@@ -2,48 +2,42 @@ package packzin;
 
 import java.util.Scanner;
 
-
-
 public class MeuFi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Oi, meu fi! Quer bulacha?");
-        String respostaBulacha = scanner.nextLine().toLowerCase();
+        String resposta = scanner.nextLine().toLowerCase();
         
-        if (respostaBulacha.equals("quero")) {
-            System.out.println("Aqui está a bulachinha, meu fi!");
+        if (resposta.equals("quero")) {
+            System.out.println("Valeu, meu fi! Aproveita a bulacha!");
         } else {
-            System.out.println("Tá bom, meu fi...");
-            return;
-        }
-        
-        System.out.println("Agora, meu fi, quer leite?");
-        String respostaLeite = scanner.nextLine().toLowerCase();
-        
-        if (respostaLeite.equals("quero")) {
-            System.out.println("Aqui está o leitinho quente, meu fi!");
-        } else {
-            System.out.println("Tudo bem, meu fi. E a fome, tá grande?");
-            String respostaComida = scanner.nextLine().toLowerCase();
+            System.out.println("Blz, meu fi! Quer leite?");
+            resposta = scanner.nextLine().toLowerCase();
             
-            if (respostaComida.equals("quero")) {
-                System.out.println("Aqui está uma comida gostosa, meu fi!");
+            if (resposta.equals("quero")) {
+                System.out.println("Leitinho quente, meu fi! Delícia!");
             } else {
-                System.out.println("Meu fi, quer cachaça?");
-                String respostaCachaca = scanner.nextLine().toLowerCase();
+                System.out.println("Tá de boa, meu fi! Quer comer?");
+                resposta = scanner.nextLine().toLowerCase();
                 
-                if (respostaCachaca.equals("mim de papai")) {
-                    System.out.println("AUUUUUUUUUUU Baby, I’m preying on you tonight\n" +
-                                       "Hunt you down, eat you alive\n" +
-                                       "Just like animals, animals, like animals-mals");
+                if (resposta.equals("quero")) {
+                    System.out.println("Bom apetite, meu fi!");
                 } else {
-                    System.out.println("Beleza, meu fi! Vamos continuar com as opções.");
-                    main(args); // Repete o código
+                    System.out.println("Eita, meu fi! Quer cachaça?");
+                    resposta = scanner.nextLine().toLowerCase();
+                    
+                    if (resposta.equals("mim de papai")) {
+                        System.out.println("AUUUUUUUUUUU Baby, I’m preying on you tonight\n" +
+                                           "Hunt you down, eat you alive\n" +
+                                           "Just like animals, animals, like animals-mals");
+                    } else {
+                        System.out.println("Tranquilo, meu fi!");
+                    }
                 }
             }
         }
         
-        System.out.println("Valeu, meu fi! Até mais!");
+        System.out.println("🐺");
     }
 }
