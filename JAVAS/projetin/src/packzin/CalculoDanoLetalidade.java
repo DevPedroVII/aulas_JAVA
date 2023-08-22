58155403ed150f1d932242269ed2b405f2288430
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-22 19:03:10 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/CalculoDanoLetalidade.java
+++ JAVAS/projetin/src/packzin/CalculoDanoLetalidade.java
@@ -2,4 +2,19 @@ package packzin;
 
 public class CalculoDanoLetalidade {
 
+    public static double calcularDanoComLetalidade(double danoBase, double resistenciaFisicaAlvo, double letalidade) {
+        double danoSemLetalidade = danoBase * (1 - resistenciaFisicaAlvo);
+        double danoComLetalidade = danoSemLetalidade + letalidade;
+        return danoComLetalidade;
+    }
+
+    public static void main(String[] args) {
+        double danoBase = 100;  // Dano físico base do ataque ou habilidade
+        double resistenciaFisicaAlvo = 0.5;  // Resistência física do alvo (ex: 0.5 representa 50% de resistência)
+        double letalidade = 20;  // Valor de letalidade do seu campeão
+        
+        double danoCalculado = calcularDanoComLetalidade(danoBase, resistenciaFisicaAlvo, letalidade);
+        
+        System.out.println("Dano causado com letalidade: " + danoCalculado);
+    }
 }
