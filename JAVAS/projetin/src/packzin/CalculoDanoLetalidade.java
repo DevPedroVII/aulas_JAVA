b1c5db75ccfd32eb41eda325048ce6539e565f92
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-22 23:00:49 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/CalculoDanoLetalidade.java
+++ JAVAS/projetin/src/packzin/CalculoDanoLetalidade.java
@@ -9,8 +9,9 @@ public class CalculoDanoLetalidade {
         System.out.print("Digite a quantidade de Letalidade do item: ");
         double letalidade = scanner.nextDouble();
 
-        System.out.print("Digite a resistência física do alvo: ");
-        double resistenciaFisica = scanner.nextDouble();
+        System.out.print("Digite a resistência física do alvo (em forma de porcentagem): ");
+        double resistenciaFisicaPercent = scanner.nextDouble();
+        double resistenciaFisica = resistenciaFisicaPercent / 100.0;
 
         System.out.print("Digite o dano físico base: ");
         double danoFisicoBase = scanner.nextDouble();
@@ -24,13 +25,8 @@ public class CalculoDanoLetalidade {
     }
 
     public static double calcularDanoLetalidade(double danoFisicoBase, double letalidade, double resistenciaFisica) {
-        // Evitando valores negativos para resistência física e letalidade
-        resistenciaFisica = Math.max(0, Math.min(1, resistenciaFisica));
-        letalidade = Math.max(0, letalidade);
-
-        // Cálculo do dano físico causado
+        // Aplicando a fórmula: Dano físico causado = Dano físico base * (1 - Resistência física do alvo) + Letalidade
         double danoCausado = danoFisicoBase * (1 - resistenciaFisica) + letalidade;
-
         return Math.max(0, danoCausado); // Evita que o dano seja negativo
     }
 }
---
+++ JAVAS/projetin/src/packzin/JogoDaVelha.java
@@ -0,0 +1,98 @@
+package packzin;
+
+import java.util.Scanner;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class JogoDaVelha {
+
+    public static void main(String[] args) {
+        char[][] tabuleiro = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+        };
+        
+        int jogadorAtual = 1; // Começa com o jogador 1
+        int pontosJogador1 = 0;
+        int pontosJogador2 = 0;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        while (pontosJogador1 < 3 && pontosJogador2 < 3) {
+            exibirTabuleiro(tabuleiro);
+            
+            System.out.println("Jogador " + jogadorAtual + ", é a sua vez.");
+            System.out.print("Informe a linha (0 a 2): ");
+            int linha = scanner.nextInt();
+            System.out.print("Informe a coluna (0 a 2): ");
+            int coluna = scanner.nextInt();
+            
+            if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2 || tabuleiro[linha][coluna] != ' ') {
+                System.out.println("Jogada inválida. Tente novamente.");
+                continue;
+            }
+            
+            tabuleiro[linha][coluna] = (jogadorAtual == 1) ? 'X' : 'O';
+            
+            if (verificarVitoria(tabuleiro, jogadorAtual)) {
+                if (jogadorAtual == 1) {
+                    pontosJogador1++;
+                } else {
+                    pontosJogador2++;
+                }
+                
+                System.out.println("Jogador " + jogadorAtual + " ganhou essa rodada!");
+                resetarTabuleiro(tabuleiro);
+            }
+            
+            jogadorAtual = (jogadorAtual == 1) ? 2 : 1; // Alternar jogadores
+        }
+        
+        if (pontosJogador1 == 3) {
+            System.out.println("Jogador 1 ganhou o jogo!");
+        } else {
+            System.out.println("Jogador 2 ganhou o jogo!");
+        }
+        
+        scanner.close();
+    }
+    
+    public static void exibirTabuleiro(char[][] tabuleiro) {
+        System.out.println("  0 1 2");
+        for (int i = 0; i < 3; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(tabuleiro[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static boolean verificarVitoria(char[][] tabuleiro, int jogador) {
+        char jogadorChar = (jogador == 1) ? 'X' : 'O';
+        
+        // Verificar linhas, colunas e diagonais
+        for (int i = 0; i < 3; i++) {
+            if ((tabuleiro[i][0] == jogadorChar && tabuleiro[i][1] == jogadorChar && tabuleiro[i][2] == jogadorChar) ||
+                (tabuleiro[0][i] == jogadorChar && tabuleiro[1][i] == jogadorChar && tabuleiro[2][i] == jogadorChar)) {
+                return true;
+            }
+        }
+        
+        if ((tabuleiro[0][0] == jogadorChar && tabuleiro[1][1] == jogadorChar && tabuleiro[2][2] == jogadorChar) ||
+            (tabuleiro[0][2] == jogadorChar && tabuleiro[1][1] == jogadorChar && tabuleiro[2][0] == jogadorChar)) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public static void resetarTabuleiro(char[][] tabuleiro) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tabuleiro[i][j] = ' ';
+            }
+        }
+    }
+}
