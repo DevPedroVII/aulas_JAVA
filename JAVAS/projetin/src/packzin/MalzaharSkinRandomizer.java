538586a22a0ec45c84e34955f4f732591c2dedfc
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-23 23:07:23 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/MalzaharSkinRandomizer.java
+++ JAVAS/projetin/src/packzin/MalzaharSkinRandomizer.java
@@ -1,5 +1,27 @@
 package packzin;
 
-public class MalzaharSkinRandomizer {
+import java.util.Random;
 
+public class MalzaharSkinRandomizer {
+    public static void main(String[] args) {
+        String[] malzaharSkins = {
+            "Malzahar",
+            "Malzahar Vizir",
+            "Malzahar Príncipe das Sombras",
+            "Malzahar Gênio da Lâmpada",
+            "Malzahar Suserano",
+            "Malzahar Dia Nevado",
+            "Malzahar Hextec",
+            "Malzahar Quebra-mundos",
+            "Melzahar Bees!",
+            "Malzahar Galante",
+            "Malzahar Três Honras"
+        };
+
+        Random random = new Random();
+        int randomIndex = random.nextInt(malzaharSkins.length);
+
+        String selectedSkin = malzaharSkins[randomIndex];
+        System.out.println("Skin selecionada: " + selectedSkin);
+    }
 }
