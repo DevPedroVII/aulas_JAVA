48fd35cd6419f66f78a2003007baf1189ee7c3c4
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-24 22:32:52 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/ContadorOK.java
+++ JAVAS/projetin/src/packzin/ContadorOK.java
@@ -1,5 +1,26 @@
 package packzin;
 
-public class ContadorOK {
+import java.util.Scanner;
 
+public class ContadorOK {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um texto: ");
+        String texto = scanner.nextLine();
+
+        int contador = contarOK(texto);
+        System.out.println("A palavra 'OK' aparece " + contador + " vezes.");
+    }
+
+    public static int contarOK(String texto) {
+        int contador = 0;
+        int index = texto.indexOf("OK");
+
+        while (index != -1) {
+            contador++;
+            index = texto.indexOf("OK", index + 2); // Avança para a próxima ocorrência
+        }
+
+        return contador;
+    }
 }
