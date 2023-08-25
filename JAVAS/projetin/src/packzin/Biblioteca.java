8a0cfabae60834cd63ee5b416ee4e4653eee6d1a
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-24 22:23:56 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/Biblioteca.java
+++ JAVAS/projetin/src/packzin/Biblioteca.java
@@ -1,5 +1,35 @@
 package packzin;
 
-public class Biblioteca {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Biblioteca {
+    private List<String> livros;
+
+    public Biblioteca() {
+        livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String nomeLivro) {
+        livros.add(nomeLivro);
+        System.out.println(nomeLivro + " foi adicionado à biblioteca.");
+    }
+
+    public void listarLivros() {
+        System.out.println("Livros na biblioteca:");
+        for (String livro : livros) {
+            System.out.println(livro);
+        }
+    }
+
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+
+        biblioteca.adicionarLivro("Dom Quixote");
+        biblioteca.adicionarLivro("1984");
+        biblioteca.adicionarLivro("O Grande Gatsby");
+
+        biblioteca.listarLivros();
+    }
 }
+
