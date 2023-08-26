a61b236ee914f5fe004b081032e81825523e472d
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-25 22:09:32 -0300

    PokemonTeamGenerator
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/PokemonGenerator.java
+++ JAVAS/projetin/src/packzin/PokemonGenerator.java
@@ -5,8 +5,25 @@ import java.util.Random;
 public class PokemonGenerator {
     public static void main(String[] args) {
         String[] pokemonList = {
-            "Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Eevee",
-            // ... adicione mais 95 Pokémon aqui
+            "Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Eevee","Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard",
+            "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree",
+            "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata",
+            "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu",
+            "Sandshrew", "Sandslash", "Nidoran♀", "Nidorina", "Nidoqueen", "Nidoran♂",
+            "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales",
+            "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume",
+            "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth",
+            "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine",
+            "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop",
+            "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool",
+            "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke",
+            "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio",
+            "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly",
+            "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler",
+            "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak",
+            "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn",
+            "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen",
+            "Seaking"
         };
 
         int numberOfPokemon = 100;
