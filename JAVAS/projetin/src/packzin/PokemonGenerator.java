570957468a7c6cf69f49bf7f0d96813dd04727aa
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-25 22:07:43 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/PokemonGenerator.java
+++ JAVAS/projetin/src/packzin/PokemonGenerator.java
@@ -1,5 +1,23 @@
 package packzin;
 
-public class PokemonGenerator {
+import java.util.Random;
 
+public class PokemonGenerator {
+    public static void main(String[] args) {
+        String[] pokemonList = {
+            "Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Eevee",
+            // ... adicione mais 95 Pokémon aqui
+        };
+
+        int numberOfPokemon = 100;
+        Random random = new Random();
+
+        System.out.println("Pokémon gerados:");
+
+        for (int i = 0; i < numberOfPokemon; i++) {
+            int randomIndex = random.nextInt(pokemonList.length);
+            String randomPokemon = pokemonList[randomIndex];
+            System.out.println((i + 1) + ". " + randomPokemon);
+        }
+    }
 }
