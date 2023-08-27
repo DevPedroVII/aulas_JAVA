ab911fa933d52f9cbbbaafc23b61d8410c3e9e52
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-26 23:06:21 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- JAVAS/projetin/src/packzin/Task.java
+++ JAVAS/projetin/src/packzin/Task.java
@@ -1,5 +1,27 @@
 package packzin;
 
 public class Task {
+    private String title;
+    private boolean completed;
 
+    public Task(String title) {
+        this.title = title;
+        this.completed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void completeTask() {
+        completed = true;
+    }
+
+    public void uncompleteTask() {
+        completed = false;
+    }
 }
