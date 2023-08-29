2edf5aff1201bc0dae32ffd9f5634855031d6b8c
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-28 21:43:54 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- SongRecomendation/src/Songsz/Main.java
+++ SongRecomendation/src/Songsz/Main.java
@@ -1,13 +1,21 @@
 package Songsz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        PlaylistRecommender recommender = new PlaylistRecommender();
+        Scanner scanner = new Scanner(System.in);
 
-        String mood = "happy";  // Pode ser "happy", "relax" ou "energetic"
+        System.out.println("Bem-vindo ao Recommender de Playlists!");
+        System.out.print("Que tipo de música você gostaria de ouvir? (Happy/Relax/Energetic) ");
+        String mood = scanner.nextLine();
+
+        PlaylistRecommender recommender = new PlaylistRecommender();
         Playlist recommendedPlaylist = recommender.recommendPlaylist(mood);
 
         System.out.println("Recomendo a playlist: " + recommendedPlaylist.getName());
         System.out.println("Link: " + recommendedPlaylist.getLink());
+
+        scanner.close();
     }
 }
