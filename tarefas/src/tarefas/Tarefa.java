930d0ba739d41b6fc8c8480adf14d603ec5d939a
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-29 23:45:19 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- tarefas/src/tarefas/Tarefa.java
+++ tarefas/src/tarefas/Tarefa.java
@@ -1,5 +1,29 @@
 package tarefas;
 
-public class Tarefa {
+class Tarefa {
+    private String titulo;
+    private String descricao;
+    private boolean concluida;
 
+    public Tarefa(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.concluida = false; // Ao criar, a tarefa não está concluída
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void marcarConcluida() {
+        concluida = true;
+    }
 }
