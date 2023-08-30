1f2d97510ddc1a31fd4fded0b9cc8562b11d020e
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-29 23:48:04 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- tarefas/src/tarefas/Main.java
+++ tarefas/src/tarefas/Main.java
@@ -1,5 +1,25 @@
 package tarefas;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        List<Tarefa> listaDeTarefas = new ArrayList<>();
+
+        // Adicionar tarefas à lista
+        listaDeTarefas.add(new Tarefa("Comprar mantimentos", "Leite, ovos, pão"));
+        listaDeTarefas.add(new Tarefa("Estudar Java", "Concluir o capítulo 5"));
+
+        // Marcar a primeira tarefa como concluída
+        listaDeTarefas.get(0).marcarConcluida();
+
+        // Exibir as tarefas e seus status
+        for (Tarefa tarefa : listaDeTarefas) {
+            System.out.println("Título: " + tarefa.getTitulo());
+            System.out.println("Descrição: " + tarefa.getDescricao());
+            System.out.println("Concluída: " + (tarefa.isConcluida() ? "Sim" : "Não"));
+            System.out.println();
+        }
+    }
 }
