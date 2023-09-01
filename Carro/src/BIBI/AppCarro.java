6508c21b8ab6d7e9ac589ac9dd4098e156a1d0c1
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-08-31 21:41:11 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- Carro/src/BIBI/AppCarro.java
+++ Carro/src/BIBI/AppCarro.java
@@ -1,37 +1,5 @@
 package BIBI;
 
-class Carro {
-    private String marca;
-    private String modelo;
-    private int ano;
-    private int velocidade;
+public class AppCarro {
 
-    public Carro(String marca, String modelo, int ano) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.ano = ano;
-        this.velocidade = 0;
-    }
-
-    public void acelerar(int incremento) {
-        velocidade += incremento;
-    }
-
-    public void frear(int decremento) {
-        velocidade -= decremento;
-        if (velocidade < 0) {
-            velocidade = 0;
-        }
-    }
-
-    public int getVelocidade() {
-        return velocidade;
-    }
-
-    public void imprimirInformacoes() {
-        System.out.println("Marca: " + marca);
-        System.out.println("Modelo: " + modelo);
-        System.out.println("Ano: " + ano);
-        System.out.println("Velocidade: " + velocidade + " km/h");
-    }
 }
