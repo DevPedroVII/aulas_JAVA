fc1af35d535e6cbb8b05376d868b638e0dd32404
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-09-01 22:55:01 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ anotações/src/module-info.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module anotações {
+	
+}
+
+/*
+Exemplo de Pacote para um Aplicativo de Gerenciamento de Biblioteca:
+
+Pacote: com.biblioteca.app
+Descrição: Este pacote contém as classes principais para um aplicativo de gerenciamento de biblioteca.
+Exemplo de Pacote para Funcionalidade de Autenticação:
+
+Pacote: com.exemplo.autenticacao
+Descrição: Este pacote agrupa classes relacionadas à autenticação de usuários no sistema.
+Exemplo de Pacote para Componentes Gráficos Personalizados:
+
+Pacote: com.exemplo.componentes
+Descrição: Este pacote contém classes que definem componentes gráficos personalizados para uso no aplicativo.
+Exemplo de Pacote para Manipulação de Dados de Clientes:
+
+Pacote: com.exemplo.dados.cliente
+Descrição: Este pacote lida com a manipulação de dados relacionados a clientes no sistema.
+Exemplo de Pacote para Testes Unitários:
+
+Pacote: com.exemplo.testes
+Descrição: Este pacote contém classes de teste unitário para verificar a funcionalidade do código.
+Exemplo de Pacote para Utilitários Genéricos:
+
+Pacote: com.exemplo.util
+Descrição: Este pacote inclui classes utilitárias genéricas que podem ser usadas em várias partes do projeto.
+Exemplo de Pacote para Manipulação de Imagens:
+
+Pacote: com.exemplo.imagens
+Descrição: Este pacote lida com a manipulação e processamento de imagens no aplicativo.
+Exemplo de Pacote para Comunicação de Rede:
+
+Pacote: com.exemplo.rede
+Descrição: Este pacote contém classes para lidar com a comunicação de rede, como solicitações HTTP ou conexões de soquete.
+
+*/
