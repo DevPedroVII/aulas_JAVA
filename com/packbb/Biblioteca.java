46a228fddb0566fbdd50e20e4084ec7af8c70ef1
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-09-03 00:02:52 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ com/packbb/Biblioteca.java
@@ -0,0 +1,35 @@
+package com.packbb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+    private List<String> livros;
+
+    public Biblioteca() {
+        livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String nomeLivro) {
+        livros.add(nomeLivro);
+        System.out.println(nomeLivro + " foi adicionado à biblioteca.");
+    }
+
+    public void listarLivros() {
+        System.out.println("Livros na biblioteca:");
+        for (String livro : livros) {
+            System.out.println(livro);
+        }
+    }
+
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+
+        biblioteca.adicionarLivro("Dom Quixote");
+        biblioteca.adicionarLivro("1984");
+        biblioteca.adicionarLivro("O Grande Gatsby");
+
+        biblioteca.listarLivros();
+    }
+}
+
