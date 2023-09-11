1947395570e84763fc0dce1cf9585f62a3bd051e
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-09-10 21:19:28 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- templateClass/src/br/com/packs/ConcreteClassB.java
+++ templateClass/src/br/com/packs/ConcreteClassB.java
@@ -1,5 +1,18 @@
 package br.com.packs;
 
-public class ConcreteClassB {
+class ConcreteClassB extends TemplateClass {
+    @Override
+    protected void step1() {
+        System.out.println("ConcreteClassB - Step 1");
+    }
 
+    @Override
+    protected void step2() {
+        System.out.println("ConcreteClassB - Step 2");
+    }
+
+    @Override
+    protected void step3() {
+        System.out.println("ConcreteClassB - Step 3 (custom implementation)");
+    }
 }
