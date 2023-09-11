a8386d2aaec51c8119944329b41e0c0636ac01e2
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-09-10 21:20:47 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- templateClass/src/br/com/packs/Main.java
+++ templateClass/src/br/com/packs/Main.java
@@ -1,5 +1,12 @@
 package br.com.packs;
 
 public class Main {
+    public static void main(String[] args) {
+        TemplateClass classA = new ConcreteClassA();
+        classA.templateMethod();
 
+        TemplateClass classB = new ConcreteClassB();
+        classB.templateMethod();
+    }
 }
+
