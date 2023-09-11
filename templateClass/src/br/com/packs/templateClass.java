ebe914b9167b2036be8e94f60533bfa9ea5d9c24
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-09-10 21:16:04 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- templateClass/src/br/com/packs/templateClass.java
+++ templateClass/src/br/com/packs/templateClass.java
@@ -1,5 +1,22 @@
 package br.com.packs;
 
-public class templateClass {
+abstract class TemplateClass {
+    public void templateMethod() {
+        // Etapa 1
+        step1();
 
+        // Etapa 2
+        step2();
+
+        // Etapa 3
+        step3();
+    }
+
+    protected abstract void step1();
+    protected abstract void step2();
+
+    // Método opcional que pode ser substituído pelas subclasses
+    protected void step3() {
+        System.out.println("Step 3 (default implementation)");
+    }
 }
