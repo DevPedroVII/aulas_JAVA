2b7a03131023a15adbf142ce98d2c316c0236ccc
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-09-11 19:24:01 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- interfacesexemple/src/br/com/interfacesexemple/IEletronico.java
+++ interfacesexemple/src/br/com/interfacesexemple/IEletronico.java
@@ -1,5 +1,12 @@
 package br.com.interfacesexemple;
 
+/*
+ * Uma Interface pode conter :
+ * Constantes;
+ * Metodos abstratos;
+ * 
+ */
+
 public interface IEletronico {
 	public String MARCA = "Geek";
 	
