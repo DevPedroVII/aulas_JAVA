77233af0189cc508b35a50de1443a3ae377ab01e
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-09-14 01:10:28 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ Bears/src/br/com/bears/UrsoPanda.java
@@ -0,0 +1,9 @@
+package br.com.bears;
+
+public class UrsoPanda extends Urso {
+    public UrsoPanda() {
+        super("Ailuropoda melanoleuca", "China", "Bambu principalmente",
+                "Cerca de 5 meses", "Preto e branco", "Solitário, comedores de bambu", "1,2 a 1,9 metros",
+                "70 a 125 kg (machos) / 70 a 85 kg (fêmeas)", "Até 20 anos");
+    }
+}
