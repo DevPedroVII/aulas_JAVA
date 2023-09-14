5971c3492970e4e2073dfa2620ca91dbf6ac2de4
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-09-14 01:11:40 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ Bears/src/br/com/bears/UrsoAndino.java
@@ -0,0 +1,9 @@
+package br.com.bears;
+
+public class UrsoAndino extends Urso {
+    public UrsoAndino() {
+        super("Tremarctos ornatus", "América do Sul", "Frutas, insetos, pequenos mamíferos",
+                "Cerca de 6 meses", "Marrom escuro com manchas brancas no peito", "Noturno, solitário", "1,2 a 1,8 metros",
+                "60 a 100 kg (machos) / 35 a 75 kg (fêmeas)", "Até 30 anos");
+    }
+}
