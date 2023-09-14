3f462685ef23908990d58fb2da0ed6979a98621e
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-09-14 01:11:03 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ Bears/src/br/com/bears/UrsoGrizzly.java
@@ -0,0 +1,9 @@
+package br.com.bears;
+
+public class UrsoGrizzly extends Urso {
+    public UrsoGrizzly() {
+        super("Ursus arctos horribilis", "América do Norte", "Peixes, frutas, carne",
+                "Cerca de 6 a 8 meses", "Marrom escuro", "Agressivo, excelentes nadadores", "1,8 a 2,8 metros",
+                "180 a 360 kg (machos) / 130 a 200 kg (fêmeas)", "Até 25 anos");
+    }
+}
