df06efe49d9d14a9f60131df8a7e2a7d7855f2f9
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-09-14 01:09:49 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ Bears/src/br/com/bears/UrsoNegro.java
@@ -0,0 +1,9 @@
+package br.com.bears;
+
+public class UrsoNegro extends Urso {
+    public UrsoNegro() {
+        super("Ursus americanus", "América do Norte", "Frutas, insetos, peixes, etc.",
+                "Cerca de 7 meses", "Preto", "Tímido, alimentação variada", "1,5 a 2,8 metros",
+                "100 a 300 kg (machos) / 80 a 180 kg (fêmeas)", "Até 30 anos");
+    }
+}
