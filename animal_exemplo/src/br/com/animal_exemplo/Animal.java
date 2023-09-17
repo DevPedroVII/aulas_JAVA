2e44d3a6e1d6bc527f662388da6ea16538abca55
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-09-17 13:34:01 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

---
+++ animal_exemplo/src/br/com/animal_exemplo/Animal.java
@@ -0,0 +1,26 @@
+package br.com.animal_exemplo;
+//Animal.java
+public abstract class Animal implements AnimalInterface {
+ private String nome;
+ private int idade;
+
+ public Animal(String nome, int idade) {
+     this.nome = nome;
+     this.idade = idade;
+ }
+
+ public String getNome() {
+     return nome;
+ }
+
+ @Override
+ public void fazerSom() {
+     System.out.println(nome + " faz um som genérico.");
+ }
+
+ @Override
+ public void mover() {
+     System.out.println(nome + " move-se de forma genérica.");
+ }
+}
+}
