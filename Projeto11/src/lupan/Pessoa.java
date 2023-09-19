8d5f5a316aaddd3a0cbc8865ece5fdedbe878ce2
pedro Vinicius <singed161@gmail.com>
2023-09-18 21:31:08 -0300

    add

--- Projeto11/src/lupan/Pessoa.java
+++ Projeto11/src/lupan/Pessoa.java
@@ -1,35 +1,5 @@
 package lupan;
 
-public class Pessoa {
-    private String nome;
-    private int idade;
+public class pessoa {
 
-    public Pessoa(String nome, int idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String novoNome) {
-        if (novoNome != null && !novoNome.isEmpty()) {
-            nome = novoNome;
-        }
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int novaIdade) {
-        if (novaIdade >= 0) {
-            idade = novaIdade;
-        }
-    }
-
-    public boolean isMaiorIdade() {
-        return idade >= 18;
-    }
 }
