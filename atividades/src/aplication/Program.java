0469d52b827bc6d13a107ea720ceb3c8fa609a8f
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-10-24 20:06:34 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- atividades/src/aplication/Program.java
+++ atividades/src/aplication/Program.java
@@ -25,10 +25,10 @@ public class Program {
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 		
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a ) * (p - x.b) * (p - x.c));
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.b ) * (p - y.b) * (p - y.c));
+		
+		double areaX = x.area();
+		
+		double areaY = y.area();
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
