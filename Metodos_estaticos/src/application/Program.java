a865babac4b93028c94b3ded91c8a006c0c54976
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-10-24 22:04:40 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- Metodos_estaticos/src/application/Program.java
+++ Metodos_estaticos/src/application/Program.java
@@ -3,10 +3,11 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import util.Calculator;
+
+
 public class Program {
 	
-	public static double PI = 3.14159;
-
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
@@ -15,25 +16,18 @@ public class Program {
 		System.out.println("Enter radius: ");
 		double radius = sc.nextDouble();
 		
-		double c = circumference(radius);
+		double c = Calculator.circumference(radius);
 		
-		double v = volume (radius);
+		double v = Calculator.volume(radius);
 		
 		System.out.printf("Circumference: %2f%n", c);
 		System.out.printf("Volume: %2f%n", v);
-		System.out.printf("PI Value: %2f%n", PI);
+		System.out.printf("PI Value: %2f%n", Calculator.PI);
 		
 		sc.close();
 
 	}
 	
-	public static double circumference (double radius) {
-		return 2.0 * PI * radius;
-	}
-
-	public static double volume (double radius) {
-		return 4.0 * PI * radius * radius * radius/ 3.0;
-	}
-
+	
 
 }
