a18e47befbf99f668c68413415d4bb51ec28bba1
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-10-24 21:20:07 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- Stock/src/aplication/Program.java
+++ Stock/src/aplication/Program.java
@@ -22,7 +22,28 @@ public class Program {
 		System.out.println("Quantity in stock :");
 		product.quantity = sc.nextInt();
 		
+		System.out.println();
 		System.out.println("Product Data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be removed from stock: ");
+		
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		
 		sc.close();
 	}
 
