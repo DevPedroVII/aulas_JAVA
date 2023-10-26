a1aeb5040239dae2868519646837e3468aec391c
Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>
2023-10-26 19:54:52 -0300

    Add files via upload
    Signed-off-by: Pedro Vinícius <112229221+DevPedroVII@users.noreply.github.com>

--- Stock/src/entities/Product.java
+++ Stock/src/entities/Product.java
@@ -1,37 +1,62 @@
-package entities;
-
-public class Product {
-	public String name;
-	public double price;
-	public int quantity;
+	package entities;
 	
-	public Product(String name, double price, int quantity) {
+		public class Product {
+			private String name;
+			private double price;
+			private int quantity;
+			
+			public Product() {
+				
+			}
+			public Product(String name, double price, int quantity) {
+				
+				this.name = name;
+				this.price = price;
+				this.quantity = quantity;
+				
+			}
+		   public Product(String name, double price) {
+				
+				this.name = name;
+				this.price = price;
+				
+			}
+		   
+			public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public double getPrice() {
+			return price;
+		}
+		public void setPrice(double price) {
+			this.price = price;
+		}
+		public int getQuantity() {
+			return quantity;
+		}
+	
+			public double totalValueInStock() {
+				return price * quantity;
+			}
+			
+			public void addProducts(int quantity) {
+						this.quantity += quantity;
+			}
+			
+			public void removeProducts(int quantity) {
+				this.quantity -= quantity;
+			}
 		
-		this.name = name;
-		this.price = price;
-		this.quantity = quantity;
-		
-	}
-	
-	public double totalValueInStock() {
-		return price * quantity;
-	}
-	
-	public void addProducts(int quantity) {
-				this.quantity += quantity;
-	}
-	
-	public void removeProducts(int quantity) {
-		this.quantity -= quantity;
-	}
-
-	public String toString() {
-		return name
-				+ ", $ "
-				+ String.format("%2f", price)
-				+", "
-				+ quantity
-				+ " unity, Total: $ "
-				+ String.format("%2f", totalValueInStock());
-	}
-}
+			public String toString() {
+				return name
+						+ ", $ "
+						+ String.format("%2f", price)
+						+", "
+						+ quantity
+						+ " unity, Total: $ "
+						+ String.format("%2f", totalValueInStock());
+			}
+		}
